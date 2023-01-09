655008bd3abdd82a8a3fe3274dd1e54a74971ba4
s-raza-t470 <12784652+t470+s-raza@users.noreply.github.com>
2023-01-10 01:00:13 +0530

    Move db init to app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///lbs.sqlite3"
+db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
