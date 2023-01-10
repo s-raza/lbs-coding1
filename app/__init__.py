9af9da3e271df2523405596c9311c28fcbca2816
s-raza-t470 <12784652+t470+s-raza@users.noreply.github.com>
2023-01-11 00:40:48 +0530

    Use settings from pydantic

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 
 from app.meters.routes import meters_bp
 from app.models import db
+from app.settings import cfg
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///lbs.sqlite3"
+    app.config["SQLALCHEMY_DATABASE_URI"] = cfg.db.sqlite_url
     db.init_app(app)
     app.register_blueprint(meters_bp)
 
--- start_app.py
+++ start_app.py
@@ -1,6 +1,7 @@
 from app import create_app
 from app.init_data import add_data
 from app.models import db
+from app.settings import cfg
 
 app = create_app()
 
@@ -8,5 +9,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.drop_all()
         db.create_all()
-        add_data()
-    app.run(host="0.0.0.0", debug=True)
+        add_data(cfg.init_data.num_meters, cfg.init_data.num_meter_readings)
+    app.run(host=cfg.app.host, port=cfg.app.port, debug=cfg.app.debug)
