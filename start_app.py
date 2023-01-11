81ed7741a9e2574de3f9a65cecab1d496ac5e943
s-raza-t470 <12784652+t470+s-raza@users.noreply.github.com>
2023-01-11 12:43:09 +0530

    Measure time to populate dummy data

--- start_app.py
+++ start_app.py
@@ -1,3 +1,4 @@
+import time
 from logging import INFO as loginfo
 
 from sqlalchemy_utils.functions import database_exists
@@ -14,11 +15,13 @@ if __name__ == "__main__":
     with app.app_context():
         if not database_exists(f"sqlite:///instance/{cfg.db.sqlite_file_name}"):
             app.logger.info(f"Creating database: {cfg.db.sqlite_file_name}")
+            start_time = time.time()
             db.create_all()
             app.logger.info(
                 f"Inserting meter data: meters: {cfg.init_data.num_meters}, "
                 f"readings per meter: {cfg.init_data.num_meter_readings}"
             )
             add_data(cfg.init_data.num_meters, cfg.init_data.num_meter_readings)
-            app.logger.info("Database created, meter data inserted")
+            end_time = round(time.time() - start_time, 2)
+            app.logger.info(f"Database created, meter data inserted in {end_time}")
     app.run(host=cfg.app.host, port=cfg.app.port, debug=cfg.app.debug)
